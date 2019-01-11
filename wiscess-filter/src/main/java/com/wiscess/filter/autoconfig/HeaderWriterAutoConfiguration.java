b710ff0b9e3130063e98ba053835614b97cf73fe
wanghai114 <wanghai114@gmail.com>
2019-01-11 08:13:16 +0800

    增加header的安全报文

--- wiscess-filter/src/main/java/com/wiscess/filter/autoconfig/HeaderWriterAutoConfiguration.java
+++ wiscess-filter/src/main/java/com/wiscess/filter/autoconfig/HeaderWriterAutoConfiguration.java
@@ -3,16 +3,26 @@ package com.wiscess.filter.autoconfig;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.web.header.HeaderWriter;
 import org.springframework.security.web.header.HeaderWriterFilter;
+import org.springframework.security.web.header.writers.CacheControlHeadersWriter;
+import org.springframework.security.web.header.writers.ContentSecurityPolicyHeaderWriter;
+import org.springframework.security.web.header.writers.ReferrerPolicyHeaderWriter;
+import org.springframework.security.web.header.writers.ReferrerPolicyHeaderWriter.ReferrerPolicy;
+import org.springframework.security.web.header.writers.XContentTypeOptionsHeaderWriter;
+import org.springframework.security.web.header.writers.XXssProtectionHeaderWriter;
 
 import com.wiscess.filter.header.P3pDisabledWriter;
+import com.wiscess.utils.StringUtils;
 
 
 public class HeaderWriterAutoConfiguration {
-	
+
+	@Value("${header.contentSecurityPolicy:}")
+	private String policyDirectives;
 	@Bean
 	public FilterRegistrationBean<HeaderWriterFilter> headerFilterRegistration() {
 		List<HeaderWriter> writers = getHeaderWriters();
@@ -30,7 +40,16 @@ public class HeaderWriterAutoConfiguration {
 	} 
 	private List<HeaderWriter> getHeaderWriters() {
 		List<HeaderWriter> writers = new ArrayList<HeaderWriter>();
-		writers.add( new P3pDisabledWriter());
+		writers.add(new P3pDisabledWriter());
+		writers.add(new XContentTypeOptionsHeaderWriter());
+		writers.add(new XXssProtectionHeaderWriter());
+		writers.add(new CacheControlHeadersWriter());
+		if(StringUtils.isEmpty(policyDirectives)){
+			writers.add(new ContentSecurityPolicyHeaderWriter("default-src 'self' data: 'unsafe-inline' 'unsafe-eval';img-src 'self' data:"));
+		}else{
+			writers.add(new ContentSecurityPolicyHeaderWriter(policyDirectives));
+		}
+		writers.add(new ReferrerPolicyHeaderWriter(ReferrerPolicy.SAME_ORIGIN));
 		return writers;
 	}
 }

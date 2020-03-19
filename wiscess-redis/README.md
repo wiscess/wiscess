9b1bf7cb1c9333494bc19393f911dba01089f966
wanghai114 <wanghai114@gmail.com>
2020-03-19 22:13:17 +0800

    创建wiscess-redis

--- wiscess-redis/README.md
+++ wiscess-redis/README.md
@@ -2,16 +2,21 @@
 
 ## 版本：v2.0
 
-#####创建时间：2020-03-19
+##### 创建时间：2020-03-19
 
-#####内容：
+### 简单介绍  
+
+自动配置Redis，并实现基于redisson的分布式锁。
+
+#### 参数配置：
+
 application.yml
 ```
 spring: 
   redis:
     database: 0
     host: 192.159.59.9
-    port: 16379
+    port: 6379
     timeout: 3000ms
     password: wiscess@123321
     jedis:
@@ -22,7 +27,15 @@ spring:
         min-idle: 20
 ```
 
+#### 添加依赖
+
 pom.xml
+```xml
+    <dependency>
+        <groupId>com.wiscess</groupId>
+        <artifactId>wiscess-redis</artifactId>
+        <version>2.0</version>
+    </dependency>
 ```
 
-```
+#### java中使用:
---
+++ wiscess-redis/src/main/java/com/wiscess/redis/RedisConfig.java
@@ -0,0 +1,132 @@
+package com.wiscess.redis;
+/**
+ * Copyright (C), 2014-2020, 北京智成卓越科技有限公司
+ * FileName: RedisConfig
+ * Author:   wh
+ * Date:     2020/3/19 19:44
+ * Description: 配置Redis
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.cache.RedisCacheWriter;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.listener.PatternTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * redis的配置文件
+ *
+ */
+@Configuration
+@EnableCaching
+@Slf4j
+public class RedisConfig extends CachingConfigurerSupport {
+
+    @Bean(name = "stringRedisTemplate")
+    public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory factory) {
+        StringRedisTemplate stringRedisTemplate = new StringRedisTemplate(factory);
+        stringRedisTemplate.setDefaultSerializer(new Jackson2JsonRedisSerializer<String>(String.class));
+        return new StringRedisTemplate(factory);
+    }
+
+    @Bean(name = "redisTemplate")
+    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory factory) {
+        RedisTemplate<Object, Object> template = new RedisTemplate<Object, Object>();
+        template.setConnectionFactory(factory);
+        Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<Object>(Object.class);
+        ObjectMapper om = new ObjectMapper();
+        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL, JsonTypeInfo.As.PROPERTY);
+        jackson2JsonRedisSerializer.setObjectMapper(om);
+        template.setKeySerializer(jackson2JsonRedisSerializer);
+        template.setValueSerializer(jackson2JsonRedisSerializer);
+        template.afterPropertiesSet();
+        return template;
+    }
+
+    /**
+     * 解决持久化乱码问题，在redistemlate中指定了持久化的策略
+     *
+     * @param redisTemplate
+     * @return
+     */
+    @Bean
+    @Primary
+    public CacheManager cacheManager(RedisConnectionFactory factory) {
+        RedisSerializer<String> redisSerializer = new StringRedisSerializer();
+        Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<Object>(Object.class);
+        ObjectMapper objectMapper = new ObjectMapper();
+        // 此项必须配置，否则会报java.lang.ClassCastException: java.util.LinkedHashMap cannot be cast to XXX
+        objectMapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL, JsonTypeInfo.As.PROPERTY);
+        jackson2JsonRedisSerializer.setObjectMapper(objectMapper);
+        // 配置序列化
+        RedisCacheConfiguration defaultCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(redisSerializer))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(jackson2JsonRedisSerializer))
+                .computePrefixWith(cacheName -> "basic:" + cacheName + ":");//设置key的前缀生成规则
+//				.computePrefixWith(new CacheKey());
+        RedisCacheManager cacheManager = new RedisCacheManager(RedisCacheWriter.nonLockingRedisCacheWriter(factory), defaultCacheConfiguration);
+        return cacheManager;
+    }
+
+    /**
+     * 分页的时候使用jackson无法反序列化
+     *
+     * @param factory
+     * @return
+     */
+    @Bean("pageCacheManager")
+    public CacheManager pageCacheManager(RedisConnectionFactory factory) {
+        RedisSerializer<String> redisSerializer = new StringRedisSerializer();
+        JdkSerializationRedisSerializer jdkSerializationRedisSerializer = new JdkSerializationRedisSerializer();
+        // 配置序列化
+        RedisCacheConfiguration defaultCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(redisSerializer))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(jdkSerializationRedisSerializer))
+                .computePrefixWith(cacheName -> "basic:" + cacheName + ":");//设置key的前缀生成规则
+//				.computePrefixWith(new CacheKey());
+//        RedisCacheManager cacheManager =
+        return new RedisCacheManager(RedisCacheWriter.nonLockingRedisCacheWriter(factory), defaultCacheConfiguration);
+    }
+
+
+    /**
+     * 配置监听适配器、消息监听容器
+     *
+     * @param connectionFactory
+     * @param listenerAdapter
+     * @return container
+     */
+    @Bean
+    public RedisMessageListenerContainer container(RedisConnectionFactory connectionFactory,
+                                                   MessageListenerAdapter listenerAdapter) {
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        container.addMessageListener(listenerAdapter, new PatternTopic("channel:sync-data"));
+        return container;
+    }
+
+}

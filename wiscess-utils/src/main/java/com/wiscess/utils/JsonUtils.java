ae64d9db8604a8ab8392560bf53eadb7cd45ca76
wanghai114 <wanghai114@gmail.com>
2020-08-24 14:29:27 +0800

    增加工具，依赖版本升级到2.2.5

--- wiscess-utils/src/main/java/com/wiscess/utils/JsonUtils.java
+++ wiscess-utils/src/main/java/com/wiscess/utils/JsonUtils.java
@@ -10,5 +10,67 @@
  */
 package com.wiscess.utils;
 
+import java.util.Map;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class JsonUtils {
+	private static final ObjectMapper mapper = new ObjectMapper();
+	
+    /**
+     * 将map对象的内容转换成json
+     * @param map
+     * @return
+     */
+    public static String mapToJson(Map map)throws Exception{
+    	// 将对象转成字符串
+		return mapper.writeValueAsString(map);
+    }
+    /**
+     *  将对象转成字符串
+     * @param obj
+     * @return
+     * @throws Exception
+     */
+    public static String objectToJson(Object obj) throws Exception {
+        return mapper.writeValueAsString(obj);
+    }
+    
+    /**
+     *  将Map转成指定的Bean
+     * @param map
+     * @param clazz
+     * @return
+     * @throws Exception
+     */
+    public static Object mapToBean(Map map, Class clazz) throws Exception {
+        return mapper.readValue(objectToJson(map), clazz);
+    }
+    /**
+     *  将Bean转成Map
+     * @param obj
+     * @return
+     * @throws Exception
+     */
+    public static Map beanToMap(Object obj) throws Exception {
+        return mapper.readValue(objectToJson(obj), Map.class);
+    }
+    
+    /**
+     * 字符串转map
+     * @param jsonString
+     * @return
+     */
+    public static Map jsonToMap(String jsonString) throws Exception{
+    	return mapper.readValue(jsonString, Map.class);
+    }
+    /**
+     * 字符串转Bean
+     * @param jsonString
+     * @return
+     */
+    public static Object jsonToBean(String jsonString,Class clazz) throws Exception{
+    	return mapper.readValue(jsonString, clazz);
+    }
+    
 }

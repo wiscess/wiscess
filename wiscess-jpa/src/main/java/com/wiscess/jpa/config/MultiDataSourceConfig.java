7387e6118a66c167807fc5e5b7786848ed010326
wanghai114 <wangh114@gmail.com>
2023-04-21 15:39:16 +0800

    修改数据源配置类条件

--- wiscess-jpa/src/main/java/com/wiscess/jpa/config/MultiDataSourceConfig.java
+++ wiscess-jpa/src/main/java/com/wiscess/jpa/config/MultiDataSourceConfig.java
@@ -6,7 +6,7 @@ import javax.sql.DataSource;
 import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
@@ -15,7 +15,6 @@ import org.springframework.boot.autoconfigure.jdbc.XADataSourceAutoConfiguration
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
 import org.springframework.context.annotation.Primary;
 import org.springframework.core.env.Environment;
@@ -26,9 +25,8 @@ import com.wiscess.utils.DesUtils;
 import com.wiscess.utils.StringUtils;
 import com.zaxxer.hikari.HikariDataSource;
 @Slf4j
-@Configuration
 @ConditionalOnClass({ DataSource.class, EmbeddedDatabaseType.class })
-@AutoConfigureBefore({ JndiDataSourceAutoConfiguration.class,
+@AutoConfiguration(before = { JndiDataSourceAutoConfiguration.class,
 	XADataSourceAutoConfiguration.class,
 	DataSourceAutoConfiguration.class })
 @EnableConfigurationProperties(DataSourceProperties.class)
